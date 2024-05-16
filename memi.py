f94b6a9c16035c3b6f17f0558637c8435770ae62
margalit55 <tsuprunmargo@gmail.com>
2024-05-16 20:49:42 +0300

    add

---
+++ memi.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот я тебе помогу сортировкой мусора {bot.user}!')
+
+@bot.command()
+async def waste_sorting (ctx):
+    await ctx.send('''желтый – пластик;
+зеленый – несортированные коммунальные отходы;
+оранжевый – опасные отходы;
+синий – макулатура;
+красный – стекло;
+серый – электрооборудование, вышедшее из строя.
+" * count_heh''')
+
+bot.run("")
